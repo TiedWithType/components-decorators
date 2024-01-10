557c651ccbf6c2e492efd604a48473f9d76a8955
TiedWithType <kamilochman79@gmail.com>
2024-01-10 14:28:09 +0100

    issue/bug commit for reproduce

--- app/app-root.component.ts
+++ app/app-root.component.ts
@@ -1,51 +1,26 @@
 import { Component } from '@core/component';
 import { eventListener } from '@core/event';
-import { Service } from '@core/service';
-import { IComponent } from '@core/types';
 import { html } from '@core/utils';
 
-@Service class serv {
-  age = 100;
-}
-
-@Component({ selector: 'app-hello', dependencies: [serv] })
-export class Hello implements IComponent {
+@Component({ selector: 'app-root' })
+export class AppRootComponent {
   name: string = 'World';
 
-  constructor(private s: serv ) {}
-
   get render(): string {
-    return html`<h1>Hello 345 ${this.s.age}!!!</h1>`;
+    return `<h1>Hello ${this.name}!!!</h1><app-input></app-input>`;
   }
 }
 
-@Component({ selector: 'app-hello2' })
-export class Hello2 implements IComponent {
-  name: string = 'World 2';
+@Component({
+  selector: 'app-input'
+}) export class AppInput {
+  content = '';
 
   get render(): string {
-    return html`<h2>Hello ${this.name}!!!</h2>`;
-  }
-}
-
-@Component({ selector: 'app-root' })
-export class AppRootComponent implements IComponent {
-  get render(): string {
-    return html`
-      <app-hello></app-hello>
-      <p [forEach]="let x in ${this.arr}">got {{ x }} !!!</p>
-      <app-hello2></app-hello2>
-      <p [forEach]="let x in ${this.arr}">got {{ x }} !!!</p>
-    `;
+    return html`<input>`
   }
 
-  arr = [1, 2, 3, 'boo bobo'];
-
-  @eventListener('click') click() {
-    this.arr = ['boo!'];
-  }
-
-  @eventListener('contextmenu') click2() {
-    this.arr = ['boxo!'];
+  @eventListener('input') likeApro(target: HTMLInputElement) {
+   this.content = target.value;
   }
 }
